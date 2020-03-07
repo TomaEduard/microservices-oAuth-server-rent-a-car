c68ffb17126228a6679de0510ec0c28a0c79ed60
Eduard Toma <eduard.daniel.toma@gmail.com>
2020-03-07 17:26:23 +0200

    initial commit

---
+++ src/test/java/com/example/rentcloud/auth/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentcloud.auth.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
